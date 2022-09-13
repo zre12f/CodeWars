a39f86c2478a972c80086f32cb00827d9f3b1f25
zre12f <zre12f@gmail.com>
2022-09-13 18:41:06 +0300

    solution Remove First and Last Character Part Two

--- 6_kyu/Remove First and Last Character Part Two.py
+++ 6_kyu/Remove First and Last Character Part Two.py
@@ -1,2 +1,11 @@
 def array(string):
-    return None if string == "" or string == "1" or string == "1,2" else ' '.join(string.split(',')[1:-1])
+    if len(string.split(',')) > 3:
+        print(' '.join(string.split(',')[1:-1]))
+    elif len(string.split(',')) == 3:
+        print(string.split(',')[1])
+    else:
+        print(None)
+
+
+
+array('1, 2, 3, 4, 5')
